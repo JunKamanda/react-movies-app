88007484e0d1fbe73b65c94e850f2ea6264f51b1
JunKamanda <jkamanda062@gmail.com>
2025-11-06 04:07:00 +0100

    ✨ : Add Home and Favorite components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Home from './pages/Home';
+import Favorite from './pages/Favorite';
 
 const App = () => {
   return (
     <div>
       <h1>React Movies - connect to vercel</h1>
+      <Home/>
+      <Favorite/>
     </div>
   );
 };
