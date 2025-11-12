35dd45d3dc361915d7b4991766fe9e6e911eb24c
JunKamanda <jkamanda062@gmail.com>
2025-11-12 03:40:47 +0100

    ✨ F01 : HEADER > Add routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Home from './pages/Home';
 import Favorite from './pages/Favorite';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <h1>React Movies - connect to vercel</h1>
-      <Home/>
-      <Favorite/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Home = () => {
+    //State
+
+    //Comportement
+
+    //Render
     return (
         <div>
+            <Header />
             <h1>Home</h1>
         </div>
     );
