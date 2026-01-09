6bb15c3e52e0546cf30d7b247337ec3f83585844
JunKamanda <jkamanda062@gmail.com>
2026-01-09 10:49:35 +0100

    💅: HEADER > Navbar stylisé

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
   //State
@@ -8,10 +9,7 @@ const Header = () => {
 
   //Render
   return (
-    <div className="navbar">
-      <div className="title">
-        <h1>React Movies</h1>
-      </div>
+    <Navbar>
       <div className="nav">
         <ul>
             <NavLink to="/">
@@ -22,8 +20,54 @@ const Header = () => {
             </NavLink>
         </ul>
       </div>
-    </div>
+      <div className="title">
+        <img src="./src/assets/play.png" alt="" />
+        <h1>React Movies</h1>
+      </div>
+    </Navbar>
   );
 };
 
 export default Header;
+
+const Navbar = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2px 0 50px;
+
+  .nav{
+
+    ul{
+      display: flex;
+      align-content: center;
+      justify-content: center;
+      gap: 20px;
+
+      li{
+        padding: 5px 10px;
+        border-radius: 5px;
+        transition: .3s all ease-in;
+
+        &:hover{
+          background: #939393;
+        }
+      }
+    }
+  }
+  .title{
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    img{
+      width: 40px;
+      height: 40px;
+      object-fit: cover;
+    }
+    h1{
+      font-family: aristote;
+      font-size: 1.3rem;
+    }
+  }
+`
