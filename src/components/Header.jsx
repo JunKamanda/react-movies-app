c8dfcb0c8634de67ecaa27e1ddfc9f3ef2e4ff57
JunKamanda <jkamanda062@gmail.com>
2026-01-09 11:19:44 +0100

    ✨ F02 : HEADER > Nettoyage du code et suppression des styles inutilisés

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
 
   //Render
   return (
-<<<<<<< HEAD
     <Navbar>
       <div className="nav">
         <ul>
@@ -26,32 +25,11 @@ const Header = () => {
         <h1>React Movies</h1>
       </div>
     </Navbar>
-=======
-    <HeaderStyled>
-      <div className="nav">
-        <ul>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
-            <li>Accueil</li>
-          </NavLink>
-          <NavLink to="/favorite">
-            <li>Coup de Coeur</li>
-          </NavLink>
-        </ul>
-      </div>
-      <div className="title">
-        <h1>React Movies</h1>
-      </div>
-    </HeaderStyled>
->>>>>>> 31f42f9a7777b41c432a698a5dd79a8f99e8d71d
   );
 };
 
 export default Header;
 
-<<<<<<< HEAD
 const Navbar = styled.nav`
   display: flex;
   align-items: center;
@@ -70,6 +48,7 @@ const Navbar = styled.nav`
         padding: 5px 10px;
         border-radius: 5px;
         transition: .3s all ease-in;
+        font-size: 1.1rem;
 
         &:hover{
           background: #939393;
@@ -93,49 +72,3 @@ const Navbar = styled.nav`
     }
   }
 `
-=======
-// StyledComponent
-const HeaderStyled = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 10px;
-
-  .nav {
-    width: 45%;
-
-    ul {
-      display: flex;
-      gap: 15px;
-
-      li {
-        color: #fff;
-        padding: 5px 10px;
-        border-radius: 10px;
-        font-size: .6rem;
-      }
-    }
-
-    /* ACTIVE STYLE DU NAVLINK */
-    .active li {
-      background: #25004e;
-    }
-
-    a:hover li {
-      background: #24004e61;
-    }
-  }
-
-  .title {
-    width: 55%;
-    text-align: end;
-
-    h1 {
-      font-size: 1.5rem;
-      font-family: "aristote";
-    }
-  }
-`;
->>>>>>> 31f42f9a7777b41c432a698a5dd79a8f99e8d71d
